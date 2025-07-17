e55c9cbcc40616e5c7a52a34a2f5d215fd4dbb69
global-angelo <88774848+angelosolis@users.noreply.github.com>
2025-07-18 05:01:31 +0800

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ToastProvider } from "@/components/ui/simple-toast";
 import { AlertProvider } from "@/components/ui/custom-alert";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,6 +35,7 @@ export default function RootLayout({
             {children}
           </ToastProvider>
         </AlertProvider>
+        <Analytics />
       </body>
     </html>
   );
